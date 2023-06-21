3eab18b0a898dc11e316a5809a4ae2d8e47c9580
wakandee <koechallanhi@gmail.com>
2023-06-21 10:42:28 -0700

    putchar update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,13 @@
 #include "main.h"
-int main(){
-printf("_putchar")
-return 1;
+int main(void){
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return 0;
 }
