0c0f454ecd7ace84c976ec4c8114449346af4912
wakandee <koechallanhi@gmail.com>
2023-06-23 09:11:49 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,25 +5,22 @@
   *
   * Return: 10 times of the numbers since 0 up to 14
   */
-int main(void)
+void more_numbers()
 {
-void more_numbers(void)
-{
-	int x, y;
+int x, y;
 
-	for (x = 0; x < 10; x++)
+for (x = 0; x < 10; x++)
+{
+	for (y = 0; y <= 14; y++)
 	{
-		for (y = 0; y <= 14; y++)
+		if (y > 9)
 		{
-			if (y > 9)
-			{
-				_putchar((y / 10) + '0');
-			}
-
-			_putchar((y % 10) + '0');
+			_putchar((y / 10) + '0');
 		}
 
-		_putchar('\n');
+		_putchar((y % 10) + '0');
 	}
+
+	_putchar('\n');
 }
 }
