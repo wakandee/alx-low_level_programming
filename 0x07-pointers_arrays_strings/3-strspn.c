5f03ccbc7ab06a2c79a1c608bdfda503342282aa
wakandee <koechallanhi@gmail.com>
2023-07-04 15:50:38 -0700

    retunrs sub-string bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * _strspn - fn to search a string for a set of bytes
+  * @s:hold  source string
+  * @accept: holds accepted string
+  *
+  * Return: it returns number of bytes in the init segment
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0,y, t = 0;
+
+	while (accept[x])
+	{
+		y = 0;
+
+		while (s[y] != 32)
+		{
+			if (accept[x] == s[y])
+			{
+				t++;
+			}
+
+			y++;
+		}
+
+		x++;
+	}
+
+	return (t);
+}
+
