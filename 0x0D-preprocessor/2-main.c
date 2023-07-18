7911bf4c85c32629561f8b1e645224ae6c14c969
wakandee <koechallanhi@gmail.com>
2023-07-18 15:46:16 -0700

    #ifndef _HEADER_ #define _HEADER_ #define SUM(x, y) ((x) + (y))

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - fn that Prints the file name of the program
+  *
+  * Return: Always zero as usual
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
