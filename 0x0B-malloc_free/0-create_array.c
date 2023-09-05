5bc3c252ebed80aa0cbac894bc3307881e61ec4d
wakandee <koechallanhi@gmail.com>
2023-09-05 08:15:14 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 
 /*
- * create_array - funtion that create arrays of characters using malloc
+ * *create_array -  funtion that create arrays of characters using malloc
  * @size -hold the size of the array
  * @c - hold the character value
  *
  *
  */
-char *create_array(unsigned int size, char c) {
+char *create_array(unsigned int size, char c) 
+{
 	unsigned int i;
 	char *s;
 
