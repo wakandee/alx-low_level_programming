17f768a96b6668cb931ad6ed18fcbcd1401eb471
wakandee <koechallanhi@gmail.com>
2023-09-11 12:55:37 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Fn that Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
