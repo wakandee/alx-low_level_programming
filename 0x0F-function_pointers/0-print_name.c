20ac9ad5bfeaa697808ef2e2b85eb92d3f34d66d
wakandee <koechallanhi@gmail.com>
2023-09-13 16:48:51 -0700

    0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,16 @@
 #include "function_pointers.h"
-
 /**
-  * print_name - fnthat Prints a name
-  * @name: The name to prints
-  * @f: Pointer to function
-  *
-  * Return: Nothing
-  */
+ * print_name- this functions prints a name
+(* a blank line
+ *@name: string to print.
+ *@f: this a function pointer, that recieves a string.
+* Description: this function prints a name)?
+(* section header: the header of this function is function_pointers.h)*
+* Return: this is a void function no return
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
 }
