7eed6814b727127432757a12db5c833ce682dbef
wakandee <koechallanhi@gmail.com>
2024-07-15 19:18:50 +0300

    linear serch

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * linear_search - search for value in array
+  * n thi
+  * @array: pointer to array value
+  * @size: size of the array
+  * @value: size of the value.
+  *
+  * Return: the first occurence of value in array.
+  */
+int linear_search(int *array, size_t size, int value){
+  if (size == 0)
+  {
+    return -1;
+  }
+}
