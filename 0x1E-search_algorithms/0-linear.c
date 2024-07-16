b919679c88636d8ffac47e645353eeabcbbcaca1
wakandee <koechallanhi@gmail.com>
2024-07-16 19:13:53 +0300

    search algorithim final

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,17 +1,25 @@
-#include "main.h"
+#include "search_algos.h"
 
 /**
-  * linear_search - search for value in array
-  * n thi
-  * @array: pointer to array value
+  * linear_search - search for an element in an array
+  * @array: the array we want to search in
   * @size: size of the array
-  * @value: size of the value.
-  *
-  * Return: the first occurence of value in array.
-  */
-int linear_search(int *array, size_t size, int value){
-  if (size == 0)
-  {
-    return -1;
-  }
+  * @value: the target value
+  * Return: the index of the value or -1 if not found
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array || size == 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
 }
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -19,13 +19,13 @@ What is a search algorithm
 What is a linear search
 What is a binary search
 What is the best search algorithm to use depending on your needs
-Copyright - Plagiarism
+## Copyright - Plagiarism
 You are tasked to come up with solutions for the tasks below yourself to meet with the above learning objectives.
 You will not be able to meet the objectives of this or any following project by copying and pasting someone else’s work.
 You are not allowed to publish any content of this project.
 Any form of plagiarism is strictly forbidden and will result in removal from the program.
 Requirements
-General
+## General
 Allowed editors: vi, vim, emacs
 All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
 All your files should end with a new line
